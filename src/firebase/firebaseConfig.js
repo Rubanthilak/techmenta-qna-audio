b8aa804db6c7d947a2864cbaf3401bba131f6c12
Ruban Thilak <rubanthilak.24@gmail.com>
2021-04-13 00:14:51 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrtGGTF-z7nvjV7HkDE_nv2lB66_rXgkw",
-    authDomain: "techmenta-site.firebaseapp.com",
-    projectId: "techmenta-site",
-    storageBucket: "techmenta-site.appspot.com",
-    messagingSenderId: "313538870857",
-    appId: "1:313538870857:web:afe7ee19b695641b534998",
-    measurementId: "G-NSCNJHKJF9"
+   //Config here
 };
 
 firebase.initializeApp(firebaseConfig);
